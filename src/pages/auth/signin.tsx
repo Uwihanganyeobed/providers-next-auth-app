5c47c1511727a7274f962e33f2ba07f110b606f7
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-07-31 23:39:18 +0200

    all-set

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -3,17 +3,29 @@
 import { getProviders, signIn } from 'next-auth/react';
 import { GetServerSideProps } from 'next';
 
+// Define the type for the provider
+interface Provider {
+  id: string;
+  name: string;
+  type: string;
+}
+
+// Define the type for the providers object
+interface Providers {
+  [key: string]: Provider;
+}
+
 interface SignInProps {
-  providers: any;
+  providers: Providers;
 }
 
 const SignIn: React.FC<SignInProps> = ({ providers }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-6 bg-slate-700">
+    <div className="flex flex-col items-center justify-center min-h-screen py-6 bg-gray-100">
       <h1 className="text-3xl font-bold mb-6">Sign in</h1>
       <div className="space-y-4">
         {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
+          <div key={provider.id}>
             <button
               onClick={() => signIn(provider.id)}
               className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
