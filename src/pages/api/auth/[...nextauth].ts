c8a156df8df0b849b755b452f604f546ae42a7ce
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-08-01 15:02:29 +0200

    all-set

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from 'next-auth/providers/facebook';
+import LinkedInProvider from 'next-auth/providers/linkedin';
+import SpotifyProvider from 'next-auth/providers/spotify';
 import GitHubProvider from 'next-auth/providers/github';
 
 export default NextAuth({
@@ -16,9 +18,17 @@ export default NextAuth({
       clientId: "861360712077389",
       clientSecret: "1a5359086703929112e39381ef844014",
     }),
+    SpotifyProvider({
+      clientId: "e470304f3e564908bb6b9b64390aab36",
+      clientSecret: "d80158f9f6374bbba444d46192bc8b48",
+    }),
     GitHubProvider({
       clientId: "Ov23liLDmE8RDUnS1coC",
       clientSecret: "dc170c2b05247d02b24c450beea57f14bbe5603f",
+    }),
+    LinkedInProvider({
+      clientId: "77tj86m4cuj2di",
+      clientSecret: "0SO73vPpEMaYohSG",
     }),
   ],
   pages: {
