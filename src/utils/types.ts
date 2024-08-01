5667996e6711f097a74a73426eeda88b99d621e0
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-08-01 15:35:27 +0200

    all-set

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,5 @@
 // all comments of web contents
+
 /*
 Privacy Policy URL: 'https://www.privacypolicies.com/live/95fe78e5-775a-4790-af66-aca1081b7f38'
 Terms of Service URL: 'https://www.termsfeed.com/live/a254e0c5-0472-45e1-9a70-3dfb22164685'
