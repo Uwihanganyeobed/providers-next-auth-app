56c9e8d721bc5892008ad43b564256b06330b1d0
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-08-01 15:09:11 +0200

    all-set

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 
-    // src/pages/api/auth/[...nextauth].ts
+// src/pages/api/auth/[...nextauth].ts
 
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
@@ -11,24 +11,24 @@ import GitHubProvider from 'next-auth/providers/github';
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "1082702847729-scrtjar1g2c4ji116670u3nq42h9b11f.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     FacebookProvider({
-      clientId: "861360712077389",
-      clientSecret: "1a5359086703929112e39381ef844014",
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
     SpotifyProvider({
-      clientId: "e470304f3e564908bb6b9b64390aab36",
-      clientSecret: "d80158f9f6374bbba444d46192bc8b48",
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
     }),
     GitHubProvider({
-      clientId: "Ov23liLDmE8RDUnS1coC",
-      clientSecret: "dc170c2b05247d02b24c450beea57f14bbe5603f",
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
     LinkedInProvider({
-      clientId: "77tj86m4cuj2di",
-      clientSecret: "0SO73vPpEMaYohSG",
+      clientId: process.env.LINKEDIN_CLIENT_ID!,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
     }),
   ],
   pages: {
